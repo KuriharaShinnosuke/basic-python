88b0b01b0237cc77268e185f3213e78bfbbac81a
KuriharaShinnosuke <maron09060612@gmail.com>
2023-12-07 21:54:09 +0900

    Update pi.py

--- pi.py
+++ pi.py
@@ -1,4 +1,4 @@
-text = """
+mojireru = """
     How I want a drink, alcoholic of course, after the heavy chapters involving
     quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
 """
